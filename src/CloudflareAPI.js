5375a655f0ac199df0be105cdbd868ffebb943e9
toda93 <dttong93@gmail.com>
2020-03-21 21:40:36 +0700

    sync

--- src/CloudflareAPI.js
+++ src/CloudflareAPI.js
@@ -1,28 +1,54 @@
 import HttpClient from '@azteam/http-client';
+import {ErrorException} from '@azteam/error';
 
-class TelegramAPI {
+const END_POINT = 'https://api.cloudflare.com/client/v4';
+
+class CloudflareAPI {
     constructor(token) {
-        this.client = new HttpClient();
-        this.endpoint = `https://api.telegram.org/bot${token}`;
-    }
-
-    async initHook(callbackURL) {
-        return await this.client.post(this.endpoint + '/setWebhook', {
-            url: callbackURL,
+        this.token = token;
+        this.client = new HttpClient({
+            headers: {
+                'Authorization': `Bearer ${this.token}`
+            }
         });
     }
 
-    async sendInboxMessage(telegram_id, message) {
-        if(Array.isArray(message)){
-            message = message.join('\n');
+    async getListDomain() {
+        const res = await this.client.responseJSON().get(`${END_POINT}/zones`);
+        if (res.success) {
+            return res.result;
         }
+        throw new ErrorException('CLOUDFLARE_GET_LIST_DOMAIN', JSON.stringify(res));
 
-        this.client.post(this.endpoint + '/sendMessage', {
-            chat_id: telegram_id,
-            text: message,
+    }
+
+    async getDomain(name) {
+        const res = await this.client.responseJSON().get(`${END_POINT}/zones`, {
+            name
         });
-        return null;
+        if (res.success) {
+            return res.result[0];
+        }
+        throw new ErrorException('CLOUDFLARE_GET_DOMAIN', JSON.stringify(res));
+    }
+
+    async getListDNS(id) {
+        const res = await this.client.responseJSON().get(`${END_POINT}/zones/${id}/dns_records`);
+        if (res.success) {
+            return res.result;
+        }
+        throw new ErrorException('CLOUDFLARE_GET_LIST_DNS', JSON.stringify(res));
+    }
+
+    async createDNS(id, options = {}) {
+        const res = await this.client.postJSON(`${END_POINT}/zones/${id}/dns_records`, options);
+        if (res.success) {
+            return res.result;
+        }
+        console.error(JSON.stringify(res));
+        throw new ErrorException('CLOUDFLARE_CREATE_DNS', JSON.stringify(res));
+
     }
 }
 
-export default TelegramAPI;
+export default CloudflareAPI;
